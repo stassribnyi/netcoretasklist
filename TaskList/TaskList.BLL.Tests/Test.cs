428187b71514eb6e8322ce6e5636a7d44e7da05b
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-06-16 14:01:47 +0300

    Setup test project

---
+++ TaskList/TaskList.BLL.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TaskList.BLL.Tests
+{
+    [TestFixture]
+    public class TestOne
+    {
+        [Test]
+        public void TestCase ()
+        {
+            Assert.True(true);
+        }
+    }
+}
